530b6d255a31486cfca0c309fd39e7270e8d669c
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-01-15 23:37:41 +0530

    feat: add fucntionality to increment chars

---
+++ q9.js
@@ -0,0 +1,35 @@
+function replaceChar(str){
+
+    let finalStr=""
+
+    for(let i=0;i<str.length;i++){
+        req = str.charCodeAt(i)+1
+        
+        if(req==123) req=97
+        if(req==91) req = 65
+        finalStr+= String.fromCharCode(req)
+    }
+
+    return finalStr;
+
+}
+
+console.log(replaceChar("lazyinterns"))
+
+let upChar = str => {
+
+    let finalStr=""
+
+    for(let i=0;i<str.length;i++){
+        req = str.charCodeAt(i)+1
+        
+        if(req==123) req=97
+        if(req==91) req = 65
+        finalStr+= String.fromCharCode(req)
+    }
+
+    return finalStr;
+
+}
+
+console.log(upChar("codeacademy"))
