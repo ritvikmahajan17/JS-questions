8c20704212a2498d189923db4dbcede843ddffdf
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-01-15 20:44:13 +0530

    feat: add functionality to check monodigit number

---
+++ q2.js
@@ -0,0 +1,35 @@
+function isSameDigits(n){
+    let str = n.toString()
+
+    let firstChar=str[0];
+
+    for(let i=1;i<str.length;i++){
+        if(str[i]!=firstChar){
+            return "False"
+        }
+    }
+
+    return "True"
+
+
+}
+
+console.log(isSameDigits(1464896553453))
+
+let checkSameDigits = n =>{
+
+    let str = n.toString()
+
+    let firstChar=str[0];
+
+    for(let i=1;i<str.length;i++){
+        if(str[i]!=firstChar){
+            return "False"
+        }
+    }
+
+    return "True"
+
+}
+
+console.log(checkSameDigits(444444444))
