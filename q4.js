bb873c444876c00903ba0347af01b1c6e1d226f0
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-01-15 21:23:41 +0530

    feat: add functionaloty to find longest string of array

---
+++ q4.js
@@ -0,0 +1,35 @@
+let array = ["we", "love", "code", "academy"]
+
+function maxString(arr){
+
+    let maxIdx=0;
+    let maxLen=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].length>maxLen){
+            maxLen=arr[i].length
+            maxIdx=i
+        }
+    }
+
+    return arr[maxIdx]
+
+}
+
+console.log(maxString(array))
+
+let longestString = (arr) => {
+
+    let maxIdx=0;
+    let maxLen=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].length>maxLen){
+            maxLen=arr[i].length
+            maxIdx=i
+        }
+    }
+
+    return arr[maxIdx]
+
+}
+
+console.log(longestString(array))
