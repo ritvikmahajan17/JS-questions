af60ab23469d19f64453e96c785398140bc66e1a
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-01-15 20:30:42 +0530

    feat: add functionality to check max even numbers

---
+++ q1.js
@@ -0,0 +1,29 @@
+
+let array = [1, 2, 3, 4, 5]
+function maxEven(params){
+    let mx= Number.MIN_VALUE
+    
+    for(let i=0;i<params.length;i++){
+        if(params[i]%2===0){
+            mx=Math.max(params[i],mx)
+        }
+    }
+    return mx
+}
+
+console.log(maxEven(array))
+
+let maximumEven = (params) =>{
+
+    let mx= Number.MIN_VALUE
+    
+    for(let i=0;i<params.length;i++){
+        if(params[i]%2===0){
+            mx=Math.max(params[i],mx)
+        }
+    }
+    return mx
+
+}
+
+console.log(maximumEven(array))
