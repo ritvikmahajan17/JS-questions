07f7c1afcf011f9a6f75f8f24b67a9a62a128d25
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-01-16 12:16:32 +0530

    chore: rename files

---
+++ sameDigitChecker.test.js
@@ -0,0 +1,15 @@
+const {isSameDigits,checkSameDigits} = require('./q2')
+
+test('check if all digits in number are same',()=>{
+    expect(
+        isSameDigits(44444444)
+    ).toBe("True")
+}) 
+
+
+
+test('check if all digits in number are same',()=>{
+    expect(
+        checkSameDigits(434545)
+    ).toBe("False")
+}) 
